8912d74f6bc47980076feb7a259cfdb6a8ef9e8d
ilanaveh <ilana.naveh@mail.huji.ac.il>
2025-12-28 12:57:21 +0200

    replace 'np.int' with 'int'

--- mmcls/datasets/base_dataset.py
+++ mmcls/datasets/base_dataset.py
@@ -83,7 +83,7 @@ class BaseDataset(Dataset, metaclass=ABCMeta):
             int: Image category of specified index.
         """
 
-        return (int(self.data_infos[idx]['gt_label'].astype(np.int)), )
+        return (int(self.data_infos[idx]['gt_label'].astype(int)), )
 
     def prepare_data(self, idx):
         results = copy.deepcopy(self.data_infos[idx])
