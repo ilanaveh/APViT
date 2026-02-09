f7451ccdee44b502fc0f545ee13d45065a3a3af1
ilanaveh <ilana.naveh@mail.huji.ac.il>
2026-02-09 14:20:41 +0200

    In pretty_text: return text

--- utils.py
+++ utils.py
@@ -108,3 +108,5 @@ def pretty_text(cfg_dict):
         blank_line_before_nested_class_or_def=True,
         split_before_expression_after_opening_paren=True)
     text, _ = FormatCode(text, style_config=yapf_style)
+
+    return text
