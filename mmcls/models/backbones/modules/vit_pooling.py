63f972a415e155f77fd7f1fa554fc4281bb16bef
ilanaveh <ilana.naveh@mail.huji.ac.il>
2026-02-12 14:28:09 +0200

    1. PoolingAttention > forward: When saving ‘attn_weight’ and ‘keep_index’ (for hooks) - remove ‘detach’, since now they are used not only for visualization, but also for extracting attention-maps for distillation => need to add ‘detach’ in visualization code.
    2. StudentPoolingBlock:
    	* init: add use_kd bool as optional arg.
    	* forward: change tchr_attn_map to tchr_attn_weight, and add tchr_keep_index as optional arg, (also passed to self.attn).
    
    3. StudentPoolingAttention:
    	* Add dim as arg to init.
    	* forward:
    		* Change tchr_attn_map_vit to tchr_attn_weight_vit and add tchr_keep_index as optional arg.
    		* Change code to use weight instead of map (calculate weight only if use_kd=False.
    		* For validation: assert that keep_index (calculated here) == tchr_keep_index (inputted from teacher model).

--- mmcls/models/backbones/modules/vit_pooling.py
+++ mmcls/models/backbones/modules/vit_pooling.py
@@ -50,9 +50,9 @@ class PoolingAttention(nn.Module):
             keep_index = None
 
         # IN 26/1/26: Add for visualizing attention (viz_attention_vit.py)
-        self.attn_weight = attn_weight.detach()
+        self.attn_weight = attn_weight
         if keep_index is not None:
-            self.keep_index = top_pool(attn_weight, dim=1, **self.pool_config).detach()  # dim=1
+            self.keep_index = top_pool(attn_weight, dim=1, **self.pool_config)  # dim=1
         else:
             self.keep_index = None
 
@@ -101,19 +101,22 @@ class StudentPoolingBlock(PoolingBlock):
     Based on PoolingBlock.
     Used for incorporating option for distillation.
     Changes:
-        * init, self.attn = StudentPoolingAttention (instead of PoolingAttention).
-        * forward: add tchr_attn_map as optional input, and pass it to self.attn.
+        * init:
+            * Add use_kd bool as optional arg.
+            * self.attn = StudentPoolingAttention (instead of PoolingAttention).
+        * forward: add tchr_attn_weights tchr_keep_index as optional args, and pass them to self.attn.
     """
 
     def __init__(self, dim=0, num_heads=0, qkv_bias=False, qk_scale=None, drop=0., attn_drop=0., pool_config=None,
-                 *args, **kwargs):
-        super().__init__(*args, **kwargs)
+                 use_kd=False, *args, **kwargs):
+        super().__init__(dim, num_heads=num_heads, qkv_bias=qkv_bias, qk_scale=qk_scale, attn_drop=attn_drop, drop=drop,
+                         pool_config=pool_config, *args, **kwargs)
         self.attn = StudentPoolingAttention(
-            dim, num_heads=num_heads, qkv_bias=qkv_bias, qk_scale=qk_scale, attn_drop=attn_drop, proj_drop=drop,
-            pool_config=pool_config)
+            dim, use_kd=use_kd, num_heads=num_heads, qkv_bias=qkv_bias, qk_scale=qk_scale, attn_drop=attn_drop,
+            proj_drop=drop, pool_config=pool_config)
 
-    def forward(self, x, tchr_attn_map=None):
-        feature, keep_index = self.attn(self.norm1(x), tchr_attn_map)
+    def forward(self, x, tchr_attn_weight=None, tchr_keep_index=None):
+        feature, keep_index = self.attn(self.norm1(x), tchr_attn_weight, tchr_keep_index)
         x = x + self.drop_path(feature)
         if keep_index is not None:
             if len(keep_index) != x.shape[1]:
@@ -127,19 +130,22 @@ class StudentPoolingAttention(PoolingAttention):
     Based on PoolingAttention.
     Use logic similar to 'StudentPoolingViT' (in vit_siam_merge) for incorporating option for distillation:
         1. Add use_kd to init.
-        2. Modify forward to allow using tchr_attn_map for pooling.
+        2. Modify forward to allow using tchr_attn_weight for pooling.
     """
-    def __init__(self, use_kd=False, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, dim, use_kd=False, *args, **kwargs):
+        super().__init__(dim, *args, **kwargs)
         self.use_kd = use_kd
 
-    def forward(self, x, tchr_attn_map_vit=None):
+    def forward(self, x, tchr_attn_weight_vit=None, tchr_keep_index=None):
         """
         Bosed on PoolingAttention forward.
         Changes (Same logic as 'StudentPoolingViT' (in vit_siam_merge) ):
             1. [Removed additions for attention-visualization).]
-            2. Optional arg: tchr_attn_map_vit - Transformer attention-map from teacher model.
-            3. Use tchr_attn_map_vit for creating attn_weight, if self.use_kd = True.
+            2. Optional arg: tchr_attn_weight_vit - Transformer attention-weight from teacher model.
+                * as opposed to cnn, here the input is the attention-weight (not map), just because I already have the
+                    hook in vit_pooling.PoolingAttention.
+            3. Use tchr_attn_weight_vit instead of attn_weight, if self.use_kd = True.
+            4. For validation: optional arg 'tchr_keep_index' - compare to what is calculated here.
         """
         B, N, C = x.shape
         qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, C // self.num_heads)
@@ -150,24 +156,26 @@ class StudentPoolingAttention(PoolingAttention):
 
         if self.pool_config:
             if self.use_kd:
-                attn_for_pool = tchr_attn_map_vit
+                attn_weight = tchr_attn_weight_vit
             else:
-                attn_for_pool = attn
-            attn_method = self.pool_config.get('attn_method')
-            if attn_method == 'SUM_ABS_1':
-                attn_weight = attn_for_pool[:, :, 0, :].transpose(-1, -2)    # [B, token_num, head_num]
-                attn_weight = torch.sum(torch.abs(attn_weight), dim=-1).unsqueeze(-1)
-            elif attn_method == 'SUM':
-                attn_weight = attn_for_pool[:, :, 0, :].transpose(-1, -2)    # [B, token_num, head_num]
-                attn_weight = torch.sum(attn_weight, dim=-1).unsqueeze(-1)
-            elif attn_method == 'MAX':
-                attn_weight = attn_for_pool[:, :, 0, :].transpose(-1, -2)
-                attn_weight = torch.max(attn_weight, dim=-1)[0].unsqueeze(-1)
-            else:
-                raise ValueError('Invalid attn_method: %s' % attn_method)
+                attn_method = self.pool_config.get('attn_method')
+                if attn_method == 'SUM_ABS_1':
+                    attn_weight = attn[:, :, 0, :].transpose(-1, -2)    # [B, token_num, head_num]
+                    attn_weight = torch.sum(torch.abs(attn_weight), dim=-1).unsqueeze(-1)
+                elif attn_method == 'SUM':
+                    attn_weight = attn[:, :, 0, :].transpose(-1, -2)    # [B, token_num, head_num]
+                    attn_weight = torch.sum(attn_weight, dim=-1).unsqueeze(-1)
+                elif attn_method == 'MAX':
+                    attn_weight = attn[:, :, 0, :].transpose(-1, -2)
+                    attn_weight = torch.max(attn_weight, dim=-1)[0].unsqueeze(-1)
+                else:
+                    raise ValueError('Invalid attn_method: %s' % attn_method)
 
-            # attn_weight = torch.rand(attn_weight.shape, device=attn_weight.device)
+                # attn_weight = torch.rand(attn_weight.shape, device=attn_weight.device)
             keep_index = top_pool(attn_weight, dim=self.dim, **self.pool_config)
+
+            if self.use_kd and keep_index is not None:
+                assert torch.all(keep_index == tchr_keep_index)
         else:
             keep_index = None
 
