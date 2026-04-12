0e1030ddcc47a7b8dc57a3dd1f87f9b1237df007
ilanaveh <ilana.naveh@mail.huji.ac.il>
2026-04-12 10:27:46 +0300

    Add args parser, so model, img, config etc. can be chosen from arguments.

--- viz_attention_vit.py
+++ viz_attention_vit.py
@@ -2,6 +2,8 @@
 26/1/26
 Based on viz_attention_cnn.
 """
+import argparse
+
 import numpy as np
 import matplotlib.pyplot as plt
 import mmcv
@@ -11,95 +13,120 @@ from mmcls.datasets.pipelines import Compose
 from mmcls.models.utils import top_pool
 
 
-cfg = mmcv.Config.fromfile("configs/apvit/RAF.py")
-cfg.model.pretrained = None
-cfg.model.extractor.pretrained = None
-cfg.model.vit.pretrained = None
-cfg.model.vit.cnn_pool_config['keep_num'] = 160
-cfg.model.vit.vit_pool_configs['keep_rates'] = [1.] * 4 + [0.9] * 4
-cfg.model.vit.attn_before_proj = True
+def parse_args():
+    parser = argparse.ArgumentParser(description='Vizualize APViT model CNN attention')
+    parser.add_argument('--config', type=str, default="configs/apvit/RAF_run_from_terminal.py",
+                        help='config file path')
+    parser.add_argument('--keep_num', type=int, default=160, help='')
+    parser.add_argument('--img_pth', type=str, default='resources/demo.jpg',
+                        help="Image for attention visualization."
+                             "Options:"
+                             "resources/demo.jpg"
+                             "resources/test_0002_112.jpg")
+    parser.add_argument('--checkpoint_pth', type=str, default='weights/APViT_RAF-3eeecf7d.pth',
+                        help="Options:"
+                             "weights/APViT_RAF-3eeecf7d.pth - official pretrained"
+                             "work_dirs/RAF_blurX/epoch_40.pth - locally trained (X = 0/4/8...")
+    parser.add_argument('--blur', type=int, default=None)
 
-# build the model and load checkpoint
-classifier = build_classifier(cfg.model)
-# load_checkpoint(classifier, "weights/APViT_RAF-3eeecf7d.pth", map_location='cpu')  # official pretrained
-load_checkpoint(classifier, "work_dirs/RAF_blur8/epoch_40.pth", map_location='cpu')
+    args = parser.parse_args()
+    return args
 
-classifier = classifier.to("cuda")
-classifier.eval()
+def main():
+    args = parse_args()
+    cfg = mmcv.Config.fromfile(args.config)
+    cfg.model.pretrained = None
+    cfg.model.extractor.pretrained = None
+    cfg.model.vit.pretrained = None
+    cfg.model.vit.cnn_pool_config['keep_num'] = args.keep_num
+    cfg.model.vit.vit_pool_configs['keep_rates'] = [1.] * 4 + [0.9] * 4
+    cfg.model.vit.attn_before_proj = True
 
-# define the preprocess for test
-test_preprocess = Compose([
-    dict(type='Resize', size=112),
-    dict(type='Normalize',
-        mean=[123.675, 116.28, 103.53],
-        std=[58.395, 57.12, 57.375]),
-    dict(type='ImageToTensor', keys=['img']),
-    dict(type='Collect', keys=['img',])
-])
+    # build the model and load checkpoint
+    classifier = build_classifier(cfg.model)
+    load_checkpoint(classifier, args.checkpoint_pth, map_location='cpu')
+    classifier = classifier.to("cuda")
+    classifier.eval()
 
-# img = mmcv.imread('resources/test_0002_112.jpg')  # choose between 'demo.jpg' / 'test_0002_112.jpg'
-img = mmcv.imread('resources/demo.jpg')
+    # define the preprocess for test
+    trans_list = [
+        dict(type='Resize', size=112),
+        dict(type='Normalize',
+             mean=[123.675, 116.28, 103.53],
+             std=[58.395, 57.12, 57.375]),
+        dict(type='ImageToTensor', keys=['img']),
+        dict(type='Collect', keys=['img'])
+    ]
+    if args.blur:
+        trans_list = [dict(type='GaussianBlur', sigma_min=args.blur, sigma_max=args.blur)] + trans_list
 
-# preprocess the image
-data = test_preprocess(dict(img=img))
-data['img'] = data['img'][None, ...].cuda()
+    test_preprocess = Compose(trans_list)
 
-# Get vit attention-maps per block:
-vit_attn_weights = []
-vit_keep_inds = []
+    img = mmcv.imread(args.img_pth)
 
+    # preprocess the image
+    data = test_preprocess(dict(img=img))
+    data['img'] = data['img'][None, ...].cuda()
 
-# To enable following hooks, I added 'self.keep_index' and 'self.attn_weight' properties to PoolingAttention
-# (implemented in APViT.mmcls.models.backbones.modules.vit_pooling.PoolingAttention.forward)
+    # Get vit attention-maps per block:
+    vit_attn_weights = []
+    vit_keep_inds = []
 
-def hook_attn_fn(module, input, output):
-    vit_attn_weights.append(module.attn_weight.cpu())
+    # To enable following hooks, I added 'self.keep_index' and 'self.attn_weight' properties to PoolingAttention
+    # (implemented in APViT.mmcls.models.backbones.modules.vit_pooling.PoolingAttention.forward)
 
-
-def hook_keep_ind_fn(module, input, output):
-    if module.keep_index is not None:
-        vit_keep_inds.append(module.keep_index.cpu())
-    else:
-        vit_keep_inds.append(module.keep_index)
-
-
-for block in classifier.vit.blocks:
-    block.attn.register_forward_hook(hook_attn_fn)
-    block.attn.register_forward_hook(hook_keep_ind_fn)
+    def hook_attn_fn(module, input, output):
+        vit_attn_weights.append(module.attn_weight.cpu())
 
 
-# First, extract CNN attention, for consistency with indexes (vit indexes are selected only from remains from cnn):
-cnn_attn_map = classifier.extract_attn_map(data['img'])
-cnn_attn_weight = cnn_attn_map.flatten(2).transpose(2, 1)
-if cfg.model.vit.cnn_pool_config is not None:
-    cnn_keep_indexes = top_pool(cnn_attn_weight, dim=1, **cfg.model.vit.cnn_pool_config).squeeze().cpu()
+    def hook_keep_ind_fn(module, input, output):
+        if module.keep_index is not None:
+            vit_keep_inds.append(module.keep_index.cpu())
+        else:
+            vit_keep_inds.append(module.keep_index)
 
-# Forward pass:
-# out = classifier(**data, return_loss=False)
 
-# Put attention_weight values in correct positions (in original 196 vector)
-current_keep_inds = cnn_keep_indexes
-for blk in range(8):
-    plt.figure(figsize=(6, 6))
+    for block in classifier.vit.blocks:
+        block.attn.register_forward_hook(hook_attn_fn)
+        block.attn.register_forward_hook(hook_keep_ind_fn)
 
-    full_attn_weight = np.zeros(196)
-    if blk <= 4:  # First five blocks - no need to remove additional patches:
-        full_attn_weight[current_keep_inds] = vit_attn_weights[blk].detach().squeeze()[1:]  # remove cls token [index 0]
-    else:  # Last three blocks - need to remove additional patches:
-        current_keep_inds = current_keep_inds[vit_keep_inds[blk - 1].detach().squeeze()[1:] - 1]  # -1 since 1st was cls token.
-        full_attn_weight[current_keep_inds] = vit_attn_weights[blk].detach().squeeze()[1:]
-    attn_map = full_attn_weight.reshape([14, 14])
-    att_112 = np.kron(attn_map, np.ones((8, 8), dtype=int))  # duplicate each attention value to all pixels in patch
 
-    mask = (att_112 == 0)   # for creating masked_attn - so color map takes into account only the relevant patches.
-    masked_att = np.ma.masked_where(mask, att_112)
+    # First, extract CNN attention, for consistency with indexes (vit indexes are selected only from remains from cnn):
+    cnn_attn_map = classifier.extract_attn_map(data['img'])
+    cnn_attn_weight = cnn_attn_map.flatten(2).transpose(2, 1)
+    if cfg.model.vit.cnn_pool_config is not None:
+        cnn_keep_indexes = top_pool(cnn_attn_weight, dim=1, **cfg.model.vit.cnn_pool_config).squeeze().cpu()
 
-    img_masked = img.copy()
-    img_masked[~att_112.astype(bool)] = 255  # mask patches that were already removed by CNN (in white)
+    # Forward pass:
+    # out = classifier(**data, return_loss=False)
 
-    plt.imshow(img_masked[:, :, ::-1])
-    plt.imshow(masked_att, cmap='jet', alpha=.4)
+    # Put attention_weight values in correct positions (in original 196 vector)
+    current_keep_inds = cnn_keep_indexes
+    for blk in range(8):
+        plt.figure(figsize=(6, 6))
 
-    plt.title(f'Block {blk}')
+        full_attn_weight = np.zeros(196)
+        if blk <= 4:  # First five blocks - no need to remove additional patches:
+            full_attn_weight[current_keep_inds] = vit_attn_weights[blk].detach().squeeze()[1:]  # remove cls token [index 0]
+        else:  # Last three blocks - need to remove additional patches:
+            current_keep_inds = current_keep_inds[vit_keep_inds[blk - 1].detach().squeeze()[1:] - 1]  # -1 since 1st was cls token.
+            full_attn_weight[current_keep_inds] = vit_attn_weights[blk].detach().squeeze()[1:]
+        attn_map = full_attn_weight.reshape([14, 14])
+        att_112 = np.kron(attn_map, np.ones((8, 8), dtype=int))  # duplicate each attention value to all pixels in patch
 
-plt.show(block=True)
+        mask = (att_112 == 0)   # for creating masked_attn - so color map takes into account only the relevant patches.
+        masked_att = np.ma.masked_where(mask, att_112)
+
+        img_masked = img.copy()
+        img_masked[~att_112.astype(bool)] = 255  # mask patches that were already removed by CNN (in white)
+
+        plt.imshow(img_masked[:, :, ::-1])
+        plt.imshow(masked_att, cmap='jet', alpha=.4)
+
+        plt.title(f'Block {blk}')
+
+    plt.show(block=True)
+
+
+if __name__ == '__main__':
+    main()
