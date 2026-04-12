0c0bdc45bc2d48be9115ad77b2e90e84f6e65967
ilanaveh <ilana.naveh@mail.huji.ac.il>
2026-04-12 11:39:09 +0300

    In PoolingBlock & StudentPoolingBlock, changed forward s.t. attn_weight is returned (for new distillation - loss based on attention-difference)

--- mmcls/models/backbones/modules/vit_pooling.py
+++ mmcls/models/backbones/modules/vit_pooling.py
@@ -82,6 +82,8 @@ class PoolingBlock(nn.Module):
 
     def forward(self, x):
         feature, keep_index = self.attn(self.norm1(x))
+        attn_weight = self.attn.attn_weight
+
         x = x + self.drop_path(feature)
         if keep_index is not None:
             if len(keep_index) != x.shape[1]:
@@ -93,7 +95,7 @@ class PoolingBlock(nn.Module):
                 # assert torch.all(torch.eq(quick_x, x))
 
         x = x + self.drop_path(self.mlp(self.norm2(x)))
-        return x
+        return x, attn_weight
 
 
 class StudentPoolingBlock(PoolingBlock):
@@ -117,12 +119,14 @@ class StudentPoolingBlock(PoolingBlock):
 
     def forward(self, x, tchr_attn_weight=None, tchr_keep_index=None):
         feature, keep_index = self.attn(self.norm1(x), tchr_attn_weight, tchr_keep_index)
+        attn_weight = self.attn.attn_weight
+        
         x = x + self.drop_path(feature)
         if keep_index is not None:
             if len(keep_index) != x.shape[1]:
                 x = x.gather(dim=1, index=keep_index)
         x = x + self.drop_path(self.mlp(self.norm2(x)))
-        return x
+        return x, attn_weight
 
 
 class StudentPoolingAttention(PoolingAttention):
