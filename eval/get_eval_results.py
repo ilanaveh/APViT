d0b13dab66f5eec48dfe87abee708154276cc81f
ilanaveh <ilana.naveh@mail.huji.ac.il>
2026-04-27 11:56:49 +0300

    Add sys import for importing from tools, fix some bugs in dict-updating, change test blurs (for this specific run)

--- eval/get_eval_results.py
+++ eval/get_eval_results.py
@@ -3,18 +3,25 @@
 Split plot_bar_graph.py - moved here code for running test and saving results.
 """
 import mmcv
-from tools import test
 import json
-import os
 import numpy as np
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from tools import test
 
 
-save2file = True
+save2file = False
 append2existing = True
 file_name = 'results2'
 
 model_names = ['RAF_blur0', 'RAF_blur8', 'RAF_blur0-8']
 test_blurs = {m: None for m in model_names}  # None for using original config.
+test_blurs['RAF_blur0'] = 8
+test_blurs['RAF_blur8'] = 0
+test_blurs['RAF_blur0-8'] = 0
 
 if append2existing and os.path.isfile(f"{file_name}.json"):
     with open(f"{file_name}.json", "r") as f:
@@ -31,7 +38,7 @@ for m in model_names:
     if test_blurs[m] is not None:
         test_blur = test_blurs[m]
         print(f"Testing with blur: {test_blur}")
-        cur_results = test.main([cfg_pth, f'../work_dirs/{m}/epoch_40.pth', '--return_acc', '--test_blur', test_blur])
+        cur_results = test.main([cfg_pth, f'../work_dirs/{m}/epoch_40.pth', '--return_acc', '--test_blur', str(test_blur)])
 
     else:
         test_blur = cfg.data['test']['pipeline'][1]['sigma_min'] if cfg.data['test']['pipeline'][1]['type'] == 'GaussianBlur' else 0
@@ -45,11 +52,12 @@ for m in model_names:
         m_rep_file = m + f'_rep{r}' if r > 1 else m + f'_rep'
         if os.path.isdir(f'../work_dirs/{m_rep_file}'):
             rep_str = f"rep{r}"
-            results[m][rep_str] = {}
+            if rep_str not in results[m]:
+                results[m][rep_str] = {}
             print(f"{m}, {rep_str}")
             print(f"Testing with blur: {test_blur}")
             if test_blurs[m] is not None:
-                cur_results = test.main([cfg_pth, f'../work_dirs/{m_rep_file}/epoch_40.pth', '--return_acc'])
+                cur_results = test.main([cfg_pth, f'../work_dirs/{m_rep_file}/epoch_40.pth', '--return_acc', '--test_blur', str(test_blur)])
             else:
                 cur_results = test.main([cfg_pth, f'../work_dirs/{m_rep_file}/epoch_40.pth', '--return_acc'])
 
