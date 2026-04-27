d89498af55d2f4d97ebde29bdfdfb4fa951651fc
ilanaveh <ilana.naveh@mail.huji.ac.il>
2026-04-27 12:11:11 +0300

    Update means & errs dict-creation, to match new format of results dict (from get_eval_results.py): {mdl: {rep: {test_blur: X}}}

--- eval/plot_bar_graph.py
+++ eval/plot_bar_graph.py
@@ -9,16 +9,17 @@ import matplotlib.pyplot as plt
 import json
 
 file_name = 'results2'
-model_names = ['RAF_blur0', 'RAF_blur8', 'RAF_blur0-8']  # choose from: ['RAF_blur0', 'RAF_blur4', 'RAF_blur0-4', 'RAF_blur8', 'RAF_blur0-8']
+model_names = ['RAF_blur0', 'RAF_blur0-8', 'RAF_blur8']  # choose from: ['RAF_blur0', 'RAF_blur4', 'RAF_blur0-4', 'RAF_blur8', 'RAF_blur0-8']
+test_blur = 0
 
 with open(f"{file_name}.json", "r") as f:
     results = json.load(f)
 
-means = {mdl_nm: np.mean([results[k]['top-1'] for k in results.keys() if ((mdl_nm == k) or (f'{mdl_nm}_rep' in k))]) for mdl_nm in model_names}
-errs = {mdl_nm: np.std([results[k]['top-1'] for k in results.keys() if ((mdl_nm == k) or (f'{mdl_nm}_rep' in k))]) for mdl_nm in model_names}
+means = {mdl_nm: np.mean([results[k][r][f'test_{test_blur}'] for k in results.keys() for r in results[k].keys() if (mdl_nm == k)]) for mdl_nm in model_names}
+errs = {mdl_nm: np.std([results[k][r][f'test_{test_blur}'] for k in results.keys() for r in results[k].keys() if (mdl_nm == k)]) for mdl_nm in model_names}
 f, ax = plt.subplots()
 ax.bar(model_names, [means[m] for m in model_names], yerr=[errs[m] for m in model_names], align='center', alpha=0.5, ecolor='black')
+ax.set_ylim([0, 100])
 plt.show(block=True)
-
 
 print('done')
